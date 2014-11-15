5d6f8e64664ed0a08a2fc677890fd28383f3cde9
oneoneO <mycartonsky@sina.com>
2014-11-15 14:00:55 +0800

    no message

--- src/businesslogic/salebl/MockAccount.java
+++ src/businesslogic/salebl/MockAccount.java
@@ -5,10 +5,11 @@
  */
 package businesslogic.salebl;
 
+import businesslogic.accountbl.Account;
 import po.AccountPO;
 import util.ResultMessage;
 
-public class MockAccount {
+public class MockAccount extends Account{
 public ResultMessage updateAccountBySale(String name,double total){
 	AccountPO a=new AccountPO("","",0);
 	a.setBalance(a.getBalance()+total);
--- src/businesslogic/salebl/MockCommodity.java
+++ src/businesslogic/salebl/MockCommodity.java
@@ -7,11 +7,12 @@ package businesslogic.salebl;
 
 import java.util.ArrayList;
 
+import businesslogic.commoditybl.Commodity;
 import po.CommodityPO;
 import util.ResultMessage;
 import vo.CommodityLineItemVO;
 
-public class MockCommodity {
+public class MockCommodity extends Commodity{
 		public ResultMessage updateCommodityBySale(ArrayList<CommodityLineItemVO> list){
 		CommodityPO c=new CommodityPO("","","",50,0,0,0,0,0,false);
 		c.setNumber(c.getNumber()-list.get(0).number);
