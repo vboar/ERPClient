71b1bcfe01abd6f8c5556e78d010abebcc128f59
oneoneO <mycartonsky@sina.com>
2014-11-23 13:56:57 +0800

    customerbl和logbl

--- src/businesslogic/customerbl/Customer.java
+++ src/businesslogic/customerbl/Customer.java
@@ -5,8 +5,14 @@
 
 package businesslogic.customerbl;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import po.CustomerPO;
 import util.ResultMessage;
+import vo.CustomerVO;
 import businesslogic.accountbl.MockLog;
+import dataservice.datafactoryservice.DataFactoryImpl;
 
 public class Customer {
 
@@ -14,4 +20,92 @@ public class Customer {
 		MockLog log = new MockLog(content);
 		return log.add();	
 	}
+	//客户名唯一
+	public ResultMessage add(CustomerVO vo) throws RemoteException{
+		if(DataFactoryImpl.getInstance().getCustomerData().findByName(vo.name).size()==0){
+			createLog("Customer Add Error:customer exists");
+			return ResultMessage.EXIST;
+		}else{
+			//todo
+			String id=createId();
+			DataFactoryImpl.getInstance().getCustomerData().insert(new CustomerPO(id,
+					vo.category,vo.level,vo.name,vo.phoneNumber,vo.address,
+					vo.postalCode,vo.email,vo.creditLimit,vo.receivables,vo.paybles,
+					vo.defaultOperator,vo.isDeletable));
+		}
+		
+		createLog("Add customer Successfully");
+		return ResultMessage.SUCCESS;
+	}
+	
+	public ResultMessage delete(CustomerVO vo) throws RemoteException{
+		if(DataFactoryImpl.getInstance().getCustomerData().findByName(vo.name).get(0).isDeletable()){
+			DataFactoryImpl.getInstance().getCustomerData().delete(new CustomerPO(vo.id,vo.category,vo.level,vo.name,vo.phoneNumber,vo.address,
+					vo.postalCode,vo.email,vo.creditLimit,vo.receivables,vo.paybles,
+					vo.defaultOperator,vo.isDeletable));
+		}else{
+			//该客户不能删除
+			createLog("Fail to delete customer:customer is undeletable");
+			return ResultMessage.FAILED;
+		}
+		
+		createLog("Delete customer successfully");
+		return ResultMessage.SUCCESS;
+	}
+	
+	public ResultMessage update(CustomerVO vo) throws RemoteException{
+		DataFactoryImpl.getInstance().getCustomerData().update(new CustomerPO(vo.id,vo.category,vo.level,vo.name,vo.phoneNumber,vo.address,
+					vo.postalCode,vo.email,vo.creditLimit,vo.receivables,vo.paybles,
+					vo.defaultOperator,vo.isDeletable));
+		
+		createLog("Update customer successfully");
+		return ResultMessage.SUCCESS;
+	}
+	
+	public ArrayList<CustomerVO> findByname(String name) throws RemoteException{
+		ArrayList<CustomerVO> result=new ArrayList<CustomerVO>();
+		ArrayList<CustomerPO> po=DataFactoryImpl.getInstance().getCustomerData().findByName(name);
+		result=poToVo(po);
+		
+		createLog("Find customer by name successfully");
+		return result;
+	}
+	
+	public ArrayList<CustomerVO> findById(String id) throws RemoteException{
+		ArrayList<CustomerVO> result=new ArrayList<CustomerVO>();
+		ArrayList<CustomerPO> po=DataFactoryImpl.getInstance().getCustomerData().findById(id);
+		result=poToVo(po);
+		
+		createLog("Find customer by id successfully");
+		return result;
+	}
+	
+	public ArrayList<CustomerVO> show() throws RemoteException{
+		ArrayList<CustomerVO> result=new ArrayList<CustomerVO>();
+		ArrayList<CustomerPO> po=DataFactoryImpl.getInstance().getCustomerData().show();
+		result=poToVo(po);
+		
+		createLog("show successfully");
+		return result;
+	}
+	
+	//todo
+	public String createId(){
+		return null;
+	}
+	
+	public ArrayList<CustomerVO> poToVo(ArrayList<CustomerPO> po){
+		ArrayList<CustomerVO> result=new ArrayList<CustomerVO>();
+	
+		for(int i=0;i<po.size();i++){
+			CustomerPO temp=po.get(i);
+			result.add(new CustomerVO(temp.getId(),temp.getCategory(),temp.getLevel(),
+					temp.getName(),temp.getPhoneNumber(),temp.getAddress(),
+					temp.getPostalCode(),temp.getEmail(),temp.getCreditLimit(),
+					temp.getReceivables(),temp.getPaybles(),temp.getDefaultOperator(),temp.isDeletable()));
+		}
+		
+		return result;
+	}
+	
 }
--- src/businesslogic/logbl/Log.java
+++ src/businesslogic/logbl/Log.java
@@ -4,9 +4,45 @@
  */
 package businesslogic.logbl;
 
+import java.rmi.RemoteException;
+import java.util.Calendar;
+
+import po.LogPO;
+import dataservice.datafactoryservice.DataFactoryImpl;
+import util.ResultMessage;
 
 
+//oneoneO
 public class Log {
+		public ResultMessage createLog(String content) throws RemoteException{
+			Calendar c=Calendar.getInstance();
+			String year=String.valueOf(c.get(Calendar.YEAR));
+			String month=String.valueOf(c.get(Calendar.MONTH));
+			String date=String.valueOf(c.get(Calendar.DATE));
+			String hour=String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+			String minute=String.valueOf(c.get(Calendar.MINUTE));
+			String second=String.valueOf(c.get(Calendar.SECOND));
+			String time=year+"/"+month+"/"+date+" "+hour+":"+minute+":"+second;
+			
+			//todo
+			String id=getLogId();
+			
+			//todo
+			String operator=getOperator();
+			
+			DataFactoryImpl.getInstance().getLogData().insert(new LogPO(time,id,operator,content));
+			
+			return ResultMessage.SUCCESS;
+		}
 		
+		//TODO
+		public String getLogId(){
+			return null;
+		}
+		
+		//TODO
+		public String getOperator(){
+			return null;
+		}
 
 }
