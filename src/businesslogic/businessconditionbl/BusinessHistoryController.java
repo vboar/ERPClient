f3a9c0902cf15923ba2ff881a92fe9bb3f2e54fd
oneoneO <mycartonsky@sina.com>
2014-12-09 21:35:34 +0800

    经营历程查看

--- src/businesslogic/businessconditionbl/BusinessHistoryController.java
+++ src/businesslogic/businessconditionbl/BusinessHistoryController.java
@@ -3,22 +3,81 @@ package businesslogic.businessconditionbl;
 import java.util.ArrayList;
 
 import util.ResultMessage;
-import vo.DocumentVO;
+import vo.CashVO;
+import vo.ExceptionVO;
+import vo.PaymentVO;
+import vo.PurchaseVO;
 import vo.RequirementVO;
+import vo.SaleVO;
+import vo.WarningVO;
 import businesslogicservice.businessconditionblservice.HistoryBLService;
 
 public class BusinessHistoryController implements HistoryBLService {
+	BusinessHistory bh=new BusinessHistory();
+	
+	@Override
+	public ArrayList<SaleVO> showSale(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看销售单");
+		return bh.showSale(vo);
+	}
 
 	@Override
-	public ArrayList<DocumentVO> show(RequirementVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<SaleVO> showSaleReturn(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看销售退货单");
+		return bh.showSaleReturn(vo);
+	}
+	
+	@Override
+	public ArrayList<PurchaseVO> showPurchase(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看进货单");
+		return bh.showPurchase(vo);
+	}
+	
+	@Override
+	public ArrayList<PurchaseVO> showPurchaseReturn(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看销售退货单");
+		return bh.showPurchaseReturn(vo);
+	}
+
+	@Override
+	public ArrayList<PaymentVO> showPayment(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看付款单");
+		return bh.showPayment(vo);
+	}
+
+	@Override
+	public ArrayList<PaymentVO> showReceipt(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看收款单");
+		return bh.showReceipt(vo);
+	}
+
+	@Override
+	public ArrayList<CashVO> showCash(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看现金费用单单");
+		return bh.showCash(vo);
+	}
+
+	@Override
+	public ArrayList<ExceptionVO> showOverFlow(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看报溢单");
+		return bh.showOverFlow(vo);
+	}
+
+	@Override
+	public ArrayList<ExceptionVO> showLoss(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看报损单");
+		return bh.showLoss(vo);
+	}
+
+	@Override
+	public ArrayList<WarningVO> showWarning(RequirementVO vo) {
+		bh.addLog("经营历程表查看：查看报警单");
+		return bh.showWarning(vo);
 	}
 
 	@Override
 	public ResultMessage exportExcel(String path) {
-		// TODO Auto-generated method stub
+		// TODO 自动生成的方法存根
 		return null;
 	}
-
 }
--- src/businesslogic/exceptionbl/Warning.java
+++ src/businesslogic/exceptionbl/Warning.java
@@ -6,13 +6,55 @@
 
 package businesslogic.exceptionbl;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import dataservice.datafactoryservice.DataFactoryImpl;
+import po.WarningLineItemPO;
+import po.WarningPO;
+import businesslogic.logbl.Log;
+import util.DocumentType;
 import util.ResultMessage;
+import vo.WarningLineItemVO;
+import vo.WarningVO;
 
 
 public class Warning {
 	public ResultMessage createLog(String content) {
-		//TODO
-		return null;
+		Log l=new Log();
+		return l.add(content);
 	}
 
+	public ArrayList<WarningVO> findByTime(String time1,String time2){
+		ArrayList<WarningVO> result=new ArrayList<WarningVO>();
+		ArrayList<WarningPO> temp=new ArrayList<WarningPO>();
+		
+		try {
+			temp=DataFactoryImpl.getInstance().getWarningData().show(time1, time2);
+		} catch (RemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+		for(int i=0;i<temp.size();i++)
+			result.add(poToVo(temp.get(i)));
+		
+		return result;
+	}
+
+	public WarningVO poToVo(WarningPO po) {
+		ArrayList<WarningLineItemVO> list=poListToVoList(po.getList());
+		WarningVO result=new WarningVO(po.getId(),po.getTime(),
+				list,DocumentType.values()[po.getDocumentType()]);
+		return result;
+	}
+
+	public ArrayList<WarningLineItemVO> poListToVoList(
+			ArrayList<WarningLineItemPO> list) {
+		 ArrayList<WarningLineItemVO> result=new  ArrayList<WarningLineItemVO>();
+		 for(int i=0;i<list.size();i++)
+			 result.add(new WarningLineItemVO(list.get(i).getId(),list.get(i).getName(),
+					 list.get(i).getModel(),list.get(i).getStockNumber(),list.get(i).getWarningNumber()));
+		return result;
+	}
 }
